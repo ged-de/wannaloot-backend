d1e5a0b23f1c14bca33cdf341e2dfbca35d1b8d5
ged <egallwas@gmail.com>
2023-11-23 13:22:57 +0100

    introduced chopsRates model, controller, route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { GuruSheepData } from "./models/guruSheepModel.js";
 import guruSheepRoutes from "./routes/guruSheepRoutes.js";
+import chopRatesRoutes from "./routes/chopRatesRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/gurusheep", guruSheepRoutes);
+app.use("/api/choprates", chopRatesRoutes);
 
 export default app;
