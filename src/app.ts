7becfcb1ad1bed80c6bc6b1cc0fc81a7b06b7a56
ged <egallwas@gmail.com>
2023-12-21 14:45:01 +0100

    add Error-handling middleware and logger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
-import express from "express";
-import { GuruSheepData } from "./models/guruSheepModel.js";
+import express, { type Request, type Response, type NextFunction } from "express";
+
+// import { GuruSheepData } from './models/guruSheepModel.js'
 import guruSheepRoutes from "./routes/guruSheepRoutes.js";
 import chopRatesRoutes from "./routes/chopRatesRoutes.js";
-
+import { logger } from "./services/logger.js";
 const app = express();
 
 app.use(express.json());
 app.use("/api/gurusheep", guruSheepRoutes);
 app.use("/api/choprates", chopRatesRoutes);
+
+// Error-handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.stack); // Log the error for debugging
+  res.status(500).send("Internal Server Error");
+});
 
 export default app;
