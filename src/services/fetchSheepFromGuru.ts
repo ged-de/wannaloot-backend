453359c6e5184bddf5f4e95ccada9bfb877078c7
ged <egallwas@gmail.com>
2023-12-21 15:01:47 +0100

    add logger to fetch and store data from ext sources

--- src/services/fetchSheepFromGuru.ts
+++ src/services/fetchSheepFromGuru.ts
@@ -1,29 +1,38 @@
 import fetch from "node-fetch";
 import { GuruSheepData } from "../models/guruSheepModel.js";
+import { logger } from "./logger.js";
 
-export async function fetchAndStoreSheepData(GuruUrl: string) {
+export async function fetchAndStoreSheepData (GuruUrl: string): Promise<void> {
+  const headers = {
+    "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+  };
   try {
-    const headers = {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    };
     // const response = await fetch(process.env.GURU_URL as string, { headers });
     const response = await fetch(GuruUrl, { headers });
     const data = await response.json();
 
     const sheepData = new GuruSheepData(data);
-    if (sheepData.updatedAt) await sheepData.save();
+    if (typeof sheepData.updatedAt === "number" && sheepData.updatedAt > 0) {
+      await sheepData.save();
+    }
+    logger.info("Fetching and storing SheepData...");
   } catch (error) {
-    console.error("Error fetching and storing animals data:", error);
+    if (error instanceof Error) {
+      logger.error("Error fetching and storing SheepData:", error.message);
+    } else {
+      // Handle cases where the error is not an Error instance
+      logger.error("An unknown error occurred while fetching and storing SheepData:", error);
+    }
   }
 }
 
 // Function to clear the GuruSheepData collection
-export async function clearGuruSheepData() {
+export async function clearGuruSheepData (): Promise<void> {
   try {
     await GuruSheepData.deleteMany({});
-    console.log("Cleared all documents from GuruSheepData");
+    logger.info("Cleared all documents from GuruSheepData");
   } catch (error) {
-    console.error("Error clearing GuruSheepData:", error);
+    logger.error("Error clearing GuruSheepData:", error);
   }
 }
