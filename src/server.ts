b4b419e899f5791703328a8f229cb2bb235a192b
ged <egallwas@gmail.com>
2023-12-21 15:29:20 +0100

    switched from winstonLogger to global logger

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,47 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
 
 import { connectToMongo } from "./services/mongoConnection.js";
 import cron from "node-cron";
 import {
   fetchAndStoreSheepData,
-  clearGuruSheepData,
+  clearGuruSheepData
 } from "./services/fetchSheepFromGuru.js";
 import { fetchAndStoreChopRates } from "./services/fetchChopRatesFromWGP.js";
-import { fetchingLogger } from "./services/winstonLogger.js";
+import { logger } from "./services/logger.js";
 import app from "./app.js";
 
-const PORT = process.env.PORT as string;
-const MONGODB_URI = process.env.MONGODB_URI as string;
-const GURU_URL = process.env.GURU_URL as string;
-const WGP_URL = process.env.WGP_URL as string;
+dotenv.config({ path: "./.env" });
+
+const PORT = process.env.PORT;
+const MONGODB_URI2 = process.env.MONGODB_URI2;
+const GURU_URL = process.env.GURU_URL;
+const WGP_URL = process.env.WGP_URL;
 
 // console.log(process.env); // // For testing only
 
 // Connect to MongoDB
-connectToMongo(MONGODB_URI);
+if (MONGODB_URI2 === undefined || MONGODB_URI2 === "") {
+  console.error("MONGODB URI is not defined");
+  process.exit(1);
+}
+
+void connectToMongo(MONGODB_URI2);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
 // Schedule to fetch sheepData from Guru every 10 min
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
 cron.schedule("*/10 * * * *", async () => {
-  fetchingLogger.info("Fetching and storing SHEEP DATA - Task started");
+  logger.info("Fetching and storing SHEEP DATA - Task started");
   try {
     await fetchAndStoreSheepData(GURU_URL);
-    fetchingLogger.info(
+    logger.info(
       "Fetching and storing SHEEP DATA - Task completed successfully"
     );
   } catch (error) {
-    fetchingLogger.error(
+    logger.error(
       "Error during fetching and storing SHEEP DATA:",
       error
     );
@@ -42,15 +49,16 @@ cron.schedule("*/10 * * * *", async () => {
 });
 
 // Schedule to fetch chopRates from WGP every 10 min
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
 cron.schedule("*/10 * * * *", async () => {
-  fetchingLogger.info("Fetching and storing CHOP RATES - Task started");
+  logger.info("Fetching and storing CHOP RATES - Task started");
   try {
     await fetchAndStoreChopRates(WGP_URL);
-    fetchingLogger.info(
+    logger.info(
       "Fetching and storing CHOP RATES - Task completed successfully"
     );
   } catch (error) {
-    fetchingLogger.error(
+    logger.error(
       "Error during fetching and storing CHOP RATES:",
       error
     );
@@ -58,18 +66,17 @@ cron.schedule("*/10 * * * *", async () => {
 });
 
 // Schedule to clear the database to run every day at 4:55 AM Berlin Time
-cron.schedule(
-  "55 4 * * *",
-  async () => {
-    fetchingLogger.info("Clearing up mongoDB - Task started");
-    try {
-      await clearGuruSheepData();
-      fetchingLogger.info(
-        "Task completed successfully: Clearing up DB and deleting all SHEEP documents fetched from Guru"
-      );
-    } catch (error) {
-      fetchingLogger.error("Error during clearing up db:", error);
-    }
-  },
-  { timezone: "Europe/Berlin" }
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+cron.schedule("55 4 * * *", async () => {
+  logger.info("Clearing up mongoDB - Task started");
+  try {
+    await clearGuruSheepData();
+    logger.info(
+      "Task completed successfully: Clearing up DB and deleting all SHEEP documents fetched from Guru"
+    );
+  } catch (error) {
+    logger.error("Error during clearing up db:", error);
+  }
+},
+{ timezone: "Europe/Berlin" }
 );
